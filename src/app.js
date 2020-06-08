774a5e2ccc4085bed38bcfd282d98bb6c1d833dd
JTLiner925 <[jtliner925@gmail.com]>
2020-06-08 13:41:48 -0500

    committing basic heroku server

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
