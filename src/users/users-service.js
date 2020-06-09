90a68b9c6cb28651a2b3d283dce7a1a0c8386119
JTLiner925 <[jtliner925@gmail.com]>
2020-06-09 10:51:43 -0500

    cannot get the migrate to work

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,12 +1,12 @@
 const UsersService = {
   getAllUsers(knex) {
-    return knex.select('*').from('users')
+    return knex.select('*').from('one_another_users')
   },
 
   insertUser(knex, newUser) {
     return knex
       .insert(newUser)
-      .into('users')
+      .into('one_another_users')
       .returning('*')
       .then(rows => {
         return rows[0]
@@ -15,20 +15,20 @@ const UsersService = {
 
   getById(knex, id) {
     return knex
-      .from('users')
+      .from('one_another_users')
       .select('*')
       .where('id', id)
       .first()
   },
 
   deleteUser(knex, id) {
-    return knex('users')
+    return knex('one_another_users')
       .where({ id })
       .delete()
   },
 
   updateUser(knex, id, newUserFields) {
-    return knex('users')
+    return knex('one_another_users')
       .where({ id })
       .update(newUserFields)
   },
