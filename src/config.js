eee72fd74f2dcb0139a310a04e8174acf8a4ef8f
JTLiner925 <[jtliner925@gmail.com]>
2020-07-03 15:46:11 -0500

    server.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
  
-  DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/one-another",
+  DATABASE_URL: process.env.DATABASE_URL || "postgres://lqgqqqxvwnobwc:c589d6a9b536ccf574cd0ebb3d7828be28f656e7ef2791473a246cef3468d1af@ec2-54-234-44-238.compute-1.amazonaws.com:5432/dastsh6ceocfrf",
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/one-another-test'
 }
